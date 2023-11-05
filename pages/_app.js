f7c2b666853a1c3eb9df05e219af84e3c16fec2d
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-11-05 17:47:39 +0600

    Title add

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,13 @@ import { Provider } from "react-redux";
 //---INTERNAL IMPORT
 import { NavBar, Footer } from "../components/componentsIndex.js";
 import store from "@/Store/store.js";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => (
   <div>
+    <Head>
+      <title>Ollyo Image Gallery</title>
+    </Head>
     <Provider store={store}>
       <NavBar />
       <Component {...pageProps} />
