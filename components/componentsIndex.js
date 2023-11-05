d1a4a72d1653ae6abf098f868949c5be761d72d8
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-11-05 17:30:02 +0600

    all component import & export

--- components/componentsIndex.js
+++ components/componentsIndex.js
@@ -1,3 +1,8 @@
+//Module Name: Image Gallery
+//developed by:Shakil Ahmed
+//developed for:Ollyo
+//All right reserved to Shakil Ahmed
+
 import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
